180e6c141873a5d639944461936cf9d9d37aa922
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-08-15 23:54:25 -0500

    Changes To Controller, Services and View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using BlogProject.Models;
+using BlogProject.Services;
+using BlogProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,10 +14,11 @@ namespace BlogProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IBlogEmailSender _emailSender;
+        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender)
         {
             _logger = logger;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -33,6 +36,15 @@ namespace BlogProject.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Contact(ContactMe model)
+        {
+            // Location Is Where We Will Be Emailing
+            model.Message = $"{model.Message} <hr/> Phone: {model.PhoneNumber}";
+            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
+            return RedirectToAction("Index");
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ViewModels/ContactMe.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.ViewModels
+{
+    public class ContactMe
+    {
+        [Required]
+        [StringLength(75, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters long.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [StringLength(75, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters long.", MinimumLength = 2)]
+        public string Email { get; set; }
+
+
+        [Phone]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters long.", MinimumLength = 2)]
+        public string PhoneNumber { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters long.", MinimumLength = 2)]
+        public string Subject { get; set; }
+
+
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters long.", MinimumLength = 2)]
+        [Required]
+        public string Message { get; set; }
+
+
+    }
+}
